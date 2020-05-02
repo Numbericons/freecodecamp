2290be0fed3edc791b2831d5f9523f2f8088a752
Zachary <zachjoliver@gmail.com>
2020-05-01 20:50:19 -0700

    Add symetric working version

--- symetric.js
+++ symetric.js
@@ -1,13 +1,28 @@
-function sym(args) {
+function compArrs(arr1, arr2) {
   let cnt = {};
-  for (let z=0; z < arguments.length; z++) {
-    let curArg = arguments[z].sort((a,b) => a - b);
-    for (let k=0; k < curArg.length; k++) {
-      if (curArg.indexOf(curArg[z]) !== k) continue;
-      cnt[curArg[k]] ? cnt[curArg[k]] +=1 : cnt[curArg[k]] = 1; 
+  let retArr = [];
+  const len = arr1.length > arr2.length ? arr1.length : arr2.length;
+
+  for (let z = 0; z < len; z++) {
+    if (arr1[z]) {
+      if (!arr2.includes(arr1[z]) && !retArr.includes(arr1[z])) retArr.push(arr1[z]);
+      if (!retArr.includes(arr1[z]) && !arr2.includes(arr1[z])) retArr.push(arr1[z]);
+      if (arr2[z] && !retArr.includes(arr2[z]) && !arr1.includes(arr2[z])) retArr.push(arr2[z]);
+    } else {
+      if (!retArr.includes(arr2[z]) && !arr1.includes(arr2[z])) retArr.push(arr2[z]);
     }
   }
-  return Object.keys(cnt).filter(key=> cnt[key] === 1).sort((a,b) => a - b).map(el => parseInt(el));
+  return retArr.sort((a, b) => a - b);
 }
 
-sym([1, 2, 3], [5, 2, 1, 4]);
+function sym(args) {
+  let retArr = arguments[0];
+  for (let z=1; z < arguments.length; z++) {
+    retArr = compArrs(retArr, arguments[z]);
+  }
+  return retArr;
+}
+
+// sym([1, 2, 3], [5, 2, 1, 4]);
+// sym([1, 2, 5], [2, 3, 5], [3, 4, 5]) //1,4
+sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]);
